7762f12f52dd6945f6836f5af003f4dfd9aae4eb
Gecar2077 <54415576+Gecar2077@users.noreply.github.com>
2019-12-10 18:35:02 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
-print("howdy!")
+import random
+
+name = input("What is your name?")
+print("Hello " + name + "." )
+
+number = random.randint(10, 20)
+guesses = 0
+result = False
+
+start = input("Would you like to play a game? [Y/N] ")
+if start.lower() == "n":
+    print("Goodbye then")
+    exit()
+if start.lower() == "y":
+    print("I'm thinking of a number between 10 & 20")
+
+while not result:
+    guess = int(input("Have a guess: "))
+    guesses = guesses + 1
+    if guess == number:
+        result = True
+    elif guess < number:
+        print("A little Higher")
+    elif guess > number:
+        print("A little lower")
+
+print("Congrats, you guessed correctly. The number was {}".format(number))
+print("it took you {} guesses".format(guesses))
